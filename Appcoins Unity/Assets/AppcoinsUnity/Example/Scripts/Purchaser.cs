0264e4b0d59df1e05e9778b701e7d00574995dc2
Codeberg <lukmoore4all@gmail.com>
2018-04-14 23:46:04 +0100

    fully working appcoins unity plugin

---
+++ Appcoins Unity/Assets/AppcoinsUnity/Example/Scripts/Purchaser.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Codeberg.AppcoinsUnity;
+
+public class Purchaser : AppcoinsPurchaser {
+
+	public Text message;
+
+
+	void Start(){
+		message.text = "Welcome to cody's snacks shop!";
+	}
+
+	public override void purchaseSuccess (string skuid)
+	{
+		base.purchaseSuccess (skuid);
+		//purchase is successful release the product
+
+		if(skuid=="dodo"){
+		message.text="Thanks! You bought dodo";
+		}
+		else if(skuid=="monster"){
+		message.text="Thanks! You bought monster drink";
+		}
+		else if(skuid=="chocolate"){
+			message.text="Thanks! You bought chocolate";
+		}
+	}
+
+	public override void purchaseFailure (string skuid)
+	{
+		base.purchaseFailure (skuid);
+		//purchase failed perhaps show some error message
+
+		if(skuid=="dodo"){
+			message.text="Sorry! Purchase failed for dodo";
+		}
+		else if(skuid=="monster"){
+			message.text="Sorry! Purchase failed for drink";
+		}
+		else if(skuid=="chocolate"){
+			message.text="Sorry! Purchase failed for chocolate";
+		}
+	}
+
+
+	//method called when you click their respective buttons to purchase snacks
+	public void buyDodo(){
+		makePurchase ("dodo");
+	}
+
+	public void buyMonster(){
+		makePurchase ("monster");
+	}
+
+	public void buyChocolate(){
+		makePurchase ("chocolate");
+	}
+}
---
+++ Appcoins Unity/Assets/AppcoinsUnity/Scripts/AppcoinsUnity.cs
@@ -0,0 +1,71 @@
+﻿//created by Lukmon Agboola(Codeberg)
+//Note: do not change anything here as it may break the workings of the plugin else you're very sure of
+//what you're doing.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Codeberg.AppcoinsUnity{
+	
+public class AppcoinsUnity : MonoBehaviour {
+
+	[Header("Your wallet address for receiving Appcoins")]
+	public string receivingAddress;
+	[Header("Enable debug to use testnets e.g Ropsten")]
+	public bool enableDebug = false;
+	[Header("Add all your products here")]
+	public AppcoinsSku[] products;
+	[Header("Add your purchaser object here")]
+	public AppcoinsPurchaser purchaserObject;
+
+	AndroidJavaClass _class;
+	AndroidJavaObject instance { get { return _class.GetStatic<AndroidJavaObject>("instance"); } }
+
+	// Use this for initialization
+	void Start () {
+
+		//get refference to java class
+		_class = new AndroidJavaClass("com.codeberg.appcoinsunity.UnityAppcoins");
+
+		//setup wallet address
+		_class.CallStatic("setAddress",receivingAddress);
+
+		//set debug mode
+		//NOTE: this allows you to make purchases with testnets e.g Ropsten
+		_class.CallStatic("enableDebug",enableDebug);
+
+		//add all your skus here
+		addAllSKUs();
+
+		//start sdk
+		_class.CallStatic("start");
+
+	}
+
+
+	//called to add all skus specified in the inpector window.
+	private void addAllSKUs(){
+		for(int i=0; i<products.Length; i++){
+			_class.CallStatic("addNewSku",products[i].Name,products[i].SKUID,products[i].Price);
+		}
+	}
+
+	//method used in making purchase
+	public void makePurchase(string skuid){
+		_class.CallStatic("makePurchase",skuid);
+	}
+
+	//callback on successful purchases
+	public void purchaseSuccess(string skuid){
+			purchaserObject.purchaseSuccess (skuid);
+	}
+
+	//callback on failed purchases
+	public void purchaseFailure(string skuid){
+			purchaserObject.purchaseFailure (skuid);
+	}
+ }
+
+}
