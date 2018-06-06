007a5b69de9ff831cf27c88d7049ea4c6949ac5b
Nuno Monteiro <nuno.monteiro@aptoide.com>
2018-06-06 12:57:58 +0100

    New shel script to automate package creation
    Fixed issue with gitignore being local
    Added security checks on Startup script to avoid changing user data if it isn't needed

---
+++ defaultExport.sh
@@ -0,0 +1,1 @@
+export UNITY_PATH="/Applications/Unity/Hub/Editor/2018.2.0b6/Unity.app/Contents/MacOS/Unity"; export PROJ_PATH="/Users/nunomonteiro/Documents/GitHub/AppcoinsUnityPlugin/Appcoins Unity"; sh packageExporter.sh
