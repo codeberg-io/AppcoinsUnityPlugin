9350257b0e129939beb46a67295de4d8d46c3681
Nuno Monteiro <nuno.monteiro@aptoide.com>
2018-06-06 11:12:31 +0100

    Added a startup script to make sure min sdk version is 21
    Changed project version to 2018.2

---
+++ Appcoins Unity/Assets/AppcoinsUnity/Scripts/AppcoinsStartup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class Startup
+{
+    static Startup()
+    {
+        PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel21;
+        Debug.Log("Successfully integrated Appcoins Unity plugin!");
+    }
+}
