3cc4fbb1f9b7b39d2c9116a38200f98a15ac2fef
Fábio Vital <fabio.vital@aptoide.com>
2018-06-07 09:55:06 +0100

    Add mainTemplate.gradle and new appcoinsunity-release.aar

---
+++ unityappcoins/appcoinsunity/src/main/java/com/codeberg/appcoinsunity/UnityActivity.java
@@ -0,0 +1,21 @@
+package com.codeberg.appcoinsunity;
+import com.unity3d.player.UnityPlayerActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class UnityActivity extends UnityPlayerActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        // call UnityPlayerActivity.onCreate()
+        super.onCreate(savedInstanceState);
+        // print debug message to logcat
+        Log.d("UnityActivity", "Activity began.");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UnityAppcoins.instance.onActivityResult(requestCode, resultCode, data);
+    }
+}
