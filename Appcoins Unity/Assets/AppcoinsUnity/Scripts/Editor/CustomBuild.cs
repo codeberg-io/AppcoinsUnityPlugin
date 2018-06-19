6f039220e9eb27165776b7040e91f499a2fffef6
Fábio Vital <fabio.vital@aptoide.com>
2018-06-19 13:11:40 +0100

    Build for older versions is almost complete. When building the process doesnt wait till 'gradle build' ends.

--- Appcoins Unity/Assets/AppcoinsUnity/Scripts/Editor/CustomBuild.cs
+++ Appcoins Unity/Assets/AppcoinsUnity/Scripts/Editor/CustomBuild.cs
@@ -2,79 +2,178 @@
 using UnityEngine;
 using UnityEngine.Events;
 using System;
+using System.IO;
 using System.Diagnostics;
+using System.Collections;
 
-public class CustomBuild : EditorWindow
-{
-    private static bool continueBuild = false;
+public class CustomBuild : EditorWindow {
+    internal static UnityEvent continueProcessEvent = new UnityEvent();
 
-    private static UnityEvent continueProcessEvent = new UnityEvent();
-
-    // Custom class to save the loaded scenes and a bool for each scene that tells us if the user wants to export such scene or not.
-    public class SceneToExport
+    [MenuItem("Custom Build/Unix Custom Android Build")]
+    public static void CallUnixCustomBuild()
     {
-        private bool _exportScene;
-        public bool exportScene
+        CustomUnixBuild unixBuild = new CustomUnixBuild();
+        unixBuild.UnixCustomAndroidBuild();
+    }
+}
+
+public class CustomUnixBuild : CustomBuild
+{
+    private string ANDROID_STRING = "android";
+
+    public void UnixCustomAndroidBuild()
+    {
+        ExportScenes expScenes = new ExportScenes();
+        string[] scenesPath = expScenes.GetScenesToString(expScenes.GetScenesToExport());
+        CustomBuild.continueProcessEvent.AddListener(delegate{this.ExportAndBuildCustomBuildTarget("Android", scenesPath);});
+    }
+
+    private void ExportAndBuildCustomBuildTarget(string target, string[] scenesPath)
+    {
+        string path = null;
+
+        if(target.ToLower() == ANDROID_STRING)
         {
-            get { return _exportScene; }
-            set { _exportScene = value; }
+            path = this.AndroidCustomBuild(scenesPath);
         }
 
-        private UnityEngine.SceneManagement.Scene _scene;
-        public UnityEngine.SceneManagement.Scene scene 
+        if(path != null)
         {
-            get { return _scene; }
-            set { _scene = value; }
+            this.UnixBuild(path);
         }
     }
 
-    // Get all the loaded scenes and aks to the user what scenes he wants to export by 'ExportScenesWindow' class.
-    public class ExportScenes
+    private string AndroidCustomBuild(string[] scenesPath)
     {
-        private SceneToExport[] scenes;
+        return GenericBuild(scenesPath, null, BuildTarget.Android, BuildOptions.AcceptExternalModificationsToPlayer);
+    }
 
-        public static string[] GetScenesToString(SceneToExport[] scenes) 
+    private string GenericBuild (string[] scenesPath, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    {
+        string path = this.SelectPath();
+        this.VerifyIfFolderAlreadyExists(path);
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        UnityEditor.Build.Reporting.BuildReport error = BuildPipeline.BuildPlayer(scenesPath, path, build_target, build_options);
+        UnityEngine.Debug.Log(error.steps.ToString());
+        return path;
+    }
+
+    private string SelectPath() {
+        return EditorUtility.SaveFolderPanel("Save Android Project to folder", "", "");
+    }
+
+    // If folder already exists in the chosen directory delete it.
+    private void VerifyIfFolderAlreadyExists(string path)
+    {
+        string[] folders = Directory.GetDirectories(path);
+
+        for(int i = 0; i < folders.Length; i++)
         {
-            string[] pathScenes = new string[scenes.Length];
-
-            for(int i = 0; i < scenes.Length; i++)
+            if((new DirectoryInfo(folders[i]).Name) == PlayerSettings.productName)
             {
-                pathScenes[i] = scenes[i].scene.path;
-            }
+                System.IO.DirectoryInfo di = new DirectoryInfo(folders[i]);
 
-            return pathScenes;
-        }
-
-        public void getAllOpenScenes()
-        {
-            int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCount;    
-            scenes = new SceneToExport[sceneCount];
-
-            for(int i = 0; i < sceneCount; i++)
-            {
-                UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetSceneAt(i);
-
-                if(scenes[i] == null) 
+                foreach (FileInfo file in di.GetFiles())
                 {
-                    scenes[i] = new SceneToExport();
+                    file.Delete(); 
                 }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true); 
+                }
+            }
+        }
+    }
 
-                scenes[i].scene = scene;
-                scenes[i].exportScene = scene.buildIndex != -1 ? true : false;
+    private void UnixBuild(string path)
+    {
+        ProcessStartInfo ExportBuildAndRunProcess = new ProcessStartInfo("/bin/bash");
+        ExportBuildAndRunProcess.FileName = "/bin/bash";
+        ExportBuildAndRunProcess.Arguments = "-c \"cd '"  + (path + "/" + PlayerSettings.productName) + "' && pwd && ls && gradle build\"";
+        ExportBuildAndRunProcess.WorkingDirectory = Path.GetDirectoryName("/bin/bash");
+        ExportBuildAndRunProcess.UseShellExecute = true;
+        ExportBuildAndRunProcess.RedirectStandardOutput = false;
+
+        Process newProcess = Process.Start(ExportBuildAndRunProcess);
+        // string strOutput = newProcess.StandardOutput.ReadToEnd();
+        newProcess.WaitForExit();
+        // UnityEngine.Debug.Log(strOutput);
+    }
+}
+
+// Custom class to save the loaded scenes and a bool for each scene that tells us if the user wants to export such scene or not.
+public class SceneToExport
+{
+    private bool _exportScene;
+    public bool exportScene
+    {
+        get { return _exportScene; }
+        set { _exportScene = value; }
+    }
+
+    private UnityEngine.SceneManagement.Scene _scene;
+    public UnityEngine.SceneManagement.Scene scene 
+    {
+        get { return _scene; }
+        set { _scene = value; }
+    }
+}
+
+// Get all the loaded scenes and aks to the user what scenes he wants to export by 'ExportScenesWindow' class.
+public class ExportScenes
+{
+    private SceneToExport[] scenes;
+
+    public string[] GetScenesToString(SceneToExport[] scenes) 
+    {
+        ArrayList pathScenes = new ArrayList();
+
+        for(int i = 0; i < scenes.Length; i++)
+        {
+            if(scenes[i].exportScene)
+            {
+                pathScenes.Add(scenes[i].scene.path);
             }
         }
 
-        // Open ExportScenesWindow window.
-        public SceneToExport[] SelectScenesToExport() 
+        return (pathScenes.ToArray(typeof(string)) as string[]);
+    }
+
+    public SceneToExport[] GetScenesToExport()
+    {
+        this.getAllOpenScenes();
+        return this.SelectScenesToExport();
+    } 
+
+    public void getAllOpenScenes()
+    {
+        int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCount;    
+        scenes = new SceneToExport[sceneCount];
+
+        for(int i = 0; i < sceneCount; i++)
         {
-            ExportScenesWindow.CreateExportScenesWindow(scenes);
-            CustomBuild.continueProcessEvent.Invoke();
-            return scenes;
+            UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetSceneAt(i);
+
+            if(scenes[i] == null) 
+            {
+                scenes[i] = new SceneToExport();
+            }
+
+            scenes[i].scene = scene;
+            scenes[i].exportScene = scene.buildIndex != -1 ? true : false;
         }
+    }
+
+    // Open ExportScenesWindow window.
+    public SceneToExport[] SelectScenesToExport() 
+    {
+        ExportScenesWindow.CreateExportScenesWindow(scenes);
+        return scenes;
     }
 
     // Draw the window for the user select what scenes he wants to export and configure player settings.
-    public class ExportScenesWindow : EditorWindow
+    private class ExportScenesWindow : EditorWindow
     {
         private SceneToExport[] scenes;
         public Vector2 scrollViewVector = Vector2.zero;
@@ -111,69 +210,14 @@ public class CustomBuild : EditorWindow
             }
             if(GUI.Button(new Rect(460, 370, 60, 20), "Cancel"))
             {
-                CustomBuild.continueBuild = false;
                 this.Close();
             }
 
             if(GUI.Button(new Rect(530, 370, 60, 20), "Confirm"))
             {
-                CustomBuild.continueBuild = true;
+                CustomBuild.continueProcessEvent.Invoke();
                 this.Close();
             }
         }
-    }
-
-    // public class InspectorWindow : EditorWindow
-    // {
-    //     public static void OpenInspectorWindow()
-    //     {
-    //         var inspectorType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
-    //         var inspectorInstance = ScriptableObject.CreateInstance(inspectorType) as EditorWindow;
-    //         inspectorInstance.Show();
-    //     }
-
-    //     void OnGUI()
-    //     {
-    //         GUILayout.Label ("GDG Breakable Object Setup Tool", EditorStyles.boldLabel);
-    //     }
-    // }
-
-    [MenuItem("Custom Build/Unix Custom Android Build")]
-    public static void UnixCustomAndroidBuild()
-    {
-        CustomBuild.continueBuild = false;
-        SceneToExport[] scenes = CustomBuild.getScenesToExport();
-        continueProcessEvent.AddListener(delegate{CustomBuild.AndroidCustomBuild(scenes);});
-
-        if(CustomBuild.continueBuild)
-        {
-            ProcessStartInfo ExportBuildAndRunProcess = new ProcessStartInfo();
-            ExportBuildAndRunProcess.FileName = "/bin/bash";
-            ExportBuildAndRunProcess.Arguments = "-c \"ls -a -l && echo HELLO WORLD\"";
-            ExportBuildAndRunProcess.UseShellExecute = false;
-            ExportBuildAndRunProcess.RedirectStandardOutput = true;
-
-            Process newProcess = Process.Start(ExportBuildAndRunProcess);
-            string strOutput = newProcess.StandardOutput.ReadToEnd();
-            newProcess.WaitForExit();
-            UnityEngine.Debug.Log(strOutput);
-        }
-    }
-
-    private static SceneToExport[] getScenesToExport() 
-    {
-        ExportScenes exportScenes = new ExportScenes();
-        exportScenes.getAllOpenScenes();
-        return exportScenes.SelectScenesToExport();
-    }
-
-    private static void AndroidCustomBuild(SceneToExport[] scenes) 
-    {
-        BuildPlayerOptions androidBuildPlayerOptions = new BuildPlayerOptions();
-        androidBuildPlayerOptions.scenes = ExportScenes.GetScenesToString(scenes);
-        androidBuildPlayerOptions.locationPathName = "AndroidProject";
-        androidBuildPlayerOptions.target = BuildTarget.Android;
-        androidBuildPlayerOptions.options = BuildOptions.None;
-        BuildPipeline.BuildPlayer(androidBuildPlayerOptions);
     }
 }
