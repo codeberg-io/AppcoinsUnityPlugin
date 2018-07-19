b25d0a74a1ce30c6c575160f28702a6127ca4968
Fábio Vital <fabio.vital@aptoide.com>
2018-07-19 12:52:12 +0100

    Parse function from build.gradle to Tree<string> initiated.

--- Appcoins Unity/Assets/AppcoinsUnity/Scripts/Editor/Node.cs
+++ Appcoins Unity/Assets/AppcoinsUnity/Scripts/Editor/Node.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 // Node of a Tree or Graph
 class Node<T>
@@ -7,20 +8,29 @@ class Node<T>
     // Item to be stored by the node
     private T _item;
 
+    // Reference to current node's father
+    private Node<T> _parent;
     // List with all childs (outer edges)
     private List<Node<T>> _childs;
 
+    // Default constructor, item is default of 'T'
+    public Node()
+    {
+        _item = default(T);
+        _parent = null;
+        _childs = new List<Node<T>>();
+    }
+
     // Constructor with a specific 'T' item
-    public Node(T item)
+    public Node(T item) : this()
     {
         _item = item;
     }
 
-    // Default constructor, item is default of 'T'
-    public Node() : this(default(T))
+    // Constructor with a specific 'T' item a spcefic parent
+    public Node(T item, Node<T> parent) : this(item)
     {
-        _item = default(T);
-        _childs = new List<Node<T>>();
+        _parent = parent;
     }
 
     // Get item of the node
@@ -33,6 +43,35 @@ class Node<T>
     public void SetItem(T item)
     {
         _item = item;
+    }
+
+    // Get refence to parent
+    public Node<T> GetParent()
+    {
+        return _parent;
+    }
+
+    // Set refence to parent
+    public void SetParent(Node<T> parent)
+    {
+        _parent = parent;
+    }
+
+    // Get child by index
+    public Node<T> GetChild(int index)
+    {
+        return _childs[index];
+    }
+
+    public int GetChildsCount()
+    {
+        return _childs.Count;
+    }
+
+    // Change child by index at childs list
+    public void SetChild(int index, Node<T> newChild)
+    {
+        _childs[index] = newChild;
     }
 
     // Apply delegate to all node's childs
@@ -66,6 +105,11 @@ class Node<T>
     }
 }
 
+public enum FileParser
+{
+    BUILD_GRADLE
+}
+
 class Tree<T>
 {
     // Height of the tree
@@ -83,6 +127,88 @@ class Tree<T>
 
     // Contructor that creates a root node with a specific item
     public Tree(T item) : this(new Node<T>(item)) {}
+
+    // Create tree from a file (T = string)
+    public static void CreateTreeFromFile(string pathToFile, FileParser fileParser)
+    {
+        StreamReader fileReader = new StreamReader(pathToFile);
+        string allFile = fileReader.ReadToEnd();
+        fileReader.Close();
+
+        if(fileParser == FileParser.BUILD_GRADLE)
+        {
+            CreateTreeFromBuildGradleFile(allFile);
+        }
+    }
+
+    // Parse build.gradle file to a tree
+    private static void CreateTreeFromBuildGradleFile(string allFile)
+    {
+        string newString = "";
+        bool newLine = true;
+
+        Node<string> currentNode = new Node<string>("root");
+        Tree<string> t = new Tree<string>(currentNode);
+
+        for(int i = 0; i < allFile.Length; i++)
+        {
+            Node<string> auxNode;
+
+            // Ignore whitespaces till first letter (or number?)
+            while(newLine)
+            {
+                if(!Char.IsDigit(allFile[i]) && !Char.IsLetter(allFile[i]))
+                {
+                    newLine = false;
+                }
+            }
+
+            // Create new node (inner node)
+            if(allFile[i].Equals("{"))
+            {
+                currentNode.AddChild(new Node<string>(newString, currentNode));
+                currentNode = currentNode.GetChild(currentNode.GetChildsCount());
+            }
+
+            // Create new child node (leaf)
+            else if(allFile[i].Equals("\n"))
+            {
+                currentNode.AddChild(new Node<string>(newString, currentNode));
+                newLine = true;
+            }
+
+            // Close node and make father the current node
+            else if(allFile[i].Equals("}"))
+            {
+                // Create new child node before closing father node
+                if(Char.IsLetter(newString[0]) || Char.IsDigit(newString[0]))
+                {
+                    currentNode.AddChild(new Node<string>(newString, currentNode));                    
+                }
+
+                currentNode = currentNode.GetParent();
+            }
+
+            // Remove spaces between punctiation marks
+            else if(allFile[i].Equals(" "))
+            {
+                if(i == allFile.Length)
+                {
+                    break;
+                }
+
+                else if(!Char.IsLetter(allFile[i - 1]) || !Char.IsLetter(allFile[i - 1]))
+                {
+                    continue;
+                }
+            }
+
+            else
+            {
+             newString = newString.Insert(newString.Length, allFile[i].ToString());
+            }
+        }
+    }
 
     // (BFS) Find a the tree path from a list of nodes (returns the last node that exists at tree and list)
     // If a path's node exists in the tree that node is removed from the 'path' list
